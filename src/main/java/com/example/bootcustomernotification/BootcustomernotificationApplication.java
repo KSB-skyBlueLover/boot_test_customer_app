c68b5a3d933ad82d578381b9c2304032fe5d0416
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2023-05-03 09:17:27 +0900

    Initial commit

---
+++ src/main/java/com/example/bootcustomernotification/BootcustomernotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootcustomernotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcustomernotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcustomernotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootcustomernotification/BootcustomernotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootcustomernotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcustomernotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
